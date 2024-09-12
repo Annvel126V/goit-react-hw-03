07ce99b4b0b424224fc4b3f77ea4525b7138498f
Anna Velychko <162054651+Annvel126V@users.noreply.github.com>
2024-09-12 12:58:16 +0200

    add useEffec

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import ContactForm from "./components/ContactForm/ContactForm";
 import SearchBox from "./components/SearchBox/SearchBox";
 import ContactList from "./components/ContactList/ContactList";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import initialContact from "./initialContact.json";
 
 function App() {
-  const [contacts, setContacts] = useState(initialContact);
+  const [contacts, setContacts] = useState(() => {
+    const storedContacts = window.localStorage.getItem("contacts");
+    if (storedContacts !== null) {
+      return JSON.parse(storedContacts);
+    }
+    return initialContact;
+  });
   const [filter, setFlter] = useState("");
 
   const addContact = (newContact) => {
@@ -23,6 +29,11 @@ function App() {
   const visibleContacts = contacts.filter((contact) =>
     contact.name.toLowerCase().includes(filter.toLowerCase())
   );
+
+  useEffect(() => {
+    window.localStorage.setItem("contacts", JSON.stringify(contacts));
+  });
+
   return (
     <div>
       <h1>Phonebook</h1>
